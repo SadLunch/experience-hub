fedc486d7d41d46d01825dd9b2cba92954e4fc63
SadLunch <73229342+SadLunch@users.noreply.github.com>
2025-07-28 18:17:52 +0100

    Chnaged backend url to a static url to see itf the problem is there.

--- src/components/useSocket.jsx
+++ src/components/useSocket.jsx
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_SERVER_URL = import.meta.env.BACKEND_URL;
+const SOCKET_SERVER_URL = "https://experience-hub-backend.onrender.com"//import.meta.env.BACKEND_URL;
 
 const socket = io(SOCKET_SERVER_URL, {
     transports: ["websocket"],
